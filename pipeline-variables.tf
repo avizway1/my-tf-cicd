28ba1241519c38ed1511edd34d3fc5c947898d1b
Avinash <avizway@avizacademy.com>
2025-12-09 06:11:08 +0530

    Remove pipeline infrastructure from EC2 code

--- pipeline-variables.tf
+++
@@ -1,9 +0,0 @@
-variable "github_repo" {
-  type        = string
-  description = "GitHub repository in format: owner/repo-name"
-}
-
-variable "github_branch" {
-  type    = string
-  default = "main"
-}
