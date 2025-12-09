89828eb62bdcb8c99f81ef7e5ff71779288a207b
Avinash Reddy Thipparthi <57447641+avizway1@users.noreply.github.com>
2025-12-09 05:40:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,22 +13,18 @@ variable "name" {
   default = "ec2-demo"
 }
 
-# reference an existing keypair by name
+# Use existing key pair
 variable "key_name" {
   type    = string
-  default = ""   # set to an existing key pair name if you already have one
+  default = "awar06-lnx"
+}
+
+# Use existing security group by name
+variable "security_group_name" {
+  type    = string
+  default = "web-sg"
 }
 
 variable "allowed_ssh_cidr" {
   type    = string
-  default = "0.0.0.0/0"   # demo only; restrict in production
-}
-
-variable "ami_filters" {
-  type = map(string)
-  default = {
-    name                = "amzn2-ami-hvm-*-x86_64-gp2"
-    owners              = "amazon"
-    most_recent         = "true"
-  }
-}
+  default = "0.0.0.0/0"
